0090c07dcdb41d806e1dd1076974bb7190e53827
Matthew C <mcheung3@albany.edu>
2023-02-03 14:32:32 -0500

    sign up form model

---
+++ front-end/code-grader-ui/src/app/app.model.ts
@@ -0,0 +1,12 @@
+export class SignUpForm {
+
+    constructor(
+      public username: string,
+      public email: string,
+      public firstName: string,
+      public lastName: string,
+      public password: string,
+      public confirmPassword: string
+    ) {  }
+  
+  }
