5d8f9349887c46a256da807f403aa03767ca9d7e
Matthew C <mcheung3@albany.edu>
2023-02-28 21:53:25 -0500

    commit for now. need to clean up

--- front-end/code-grader-ui/src/app/routes/landing-page/landing-page/landing-page.component.ts
+++ front-end/code-grader-ui/src/app/routes/landing-page/landing-page/landing-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, forkJoin, mergeMap, Observable, switchMap } from 'rxjs';
 import { APPLICATION_NAME, LANDING_PAGE_STATE } from 'src/app/app.constants';
 import { LandingPageStorageService, LANDING_PAGE_STORAGE } from 'src/app/services/landing-page.service';
+import { S3StorageService } from 'src/app/services/s3-storage.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,12 +10,16 @@ import { LandingPageStorageService, LANDING_PAGE_STORAGE } from 'src/app/service
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  currentState : LANDING_PAGE_STATE | undefined;
+  currentState: LANDING_PAGE_STATE | undefined;
   appname = APPLICATION_NAME;
+  filesFromS3: string[] = [];
+  code: string;
+  filesToUpload: FileList | null = null;
 
   constructor(
-    private landingPageStorageService: LandingPageStorageService
-  ){}
+    private landingPageStorageService: LandingPageStorageService,
+    private s3StorageService: S3StorageService
+  ) { }
 
   ngOnInit(): void {
     this.currentState = LANDING_PAGE_STATE.HOME;
@@ -30,6 +36,59 @@ export class LandingPageComponent implements OnInit {
       LANDING_PAGE_STORAGE.currentState,
       LANDING_PAGE_STATE.SIGN_UP
     )
+  }
+
+  handleFileInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const files = target.files;
+    this.filesToUpload = files;
+    console.log("files ", this.filesToUpload)
+
+  }
+
+  onSubmitFiles(): void {
+    console.log("files to up load ", this.filesToUpload)
+    const uploadedFilesReceipt : Observable<any>[] = [];
+
+    // this.filesToUpload.forEach((file)=>{})
+    Array.from(this.filesToUpload).forEach(file => {
+      uploadedFilesReceipt.push(this.s3StorageService.uploadAssignments('CSI-404','Cheung Matthew', 'Assignment 1 - Testing', file.name, file));
+    });
+
+    combineLatest(uploadedFilesReceipt).subscribe((files)=>{
+      console.log("x ", files)
+    },
+    err => {
+      console.log("err ", err)
+    }
+    )
+  }
+
+  listFiles(): void {
+    this.s3StorageService.listFilesForAssignment('CSI-404', 'Cheung Matthew', 'Assignment 2 - Testing').subscribe((x)=>{
+      this.filesFromS3 = x.results;
+      console.log(this.filesFromS3)
+    })
+  }
+
+  getFiles(): void {
+    const x = this.filesFromS3.map((x: any)=>x.key);
+    console.log("get files ", this.filesFromS3)
+    this.s3StorageService.fetchFilesForAssignment(x).pipe(
+      switchMap((x)=> {
+        const y = x.map((i: any) => i.Body.text());
+
+        return forkJoin(y)
+      })
+    ).subscribe((z : any)=> {
+      console.log("z ", z)
+      this.code = z[0]
+    })
+  }
+
+  removeFiles(): void {
+    const x = this.filesFromS3.map((i: any) => i.key);
+    combineLatest(this.s3StorageService.removeFiles(x)).subscribe((x)=>console.log("remove ", x))
   }
 
 }
