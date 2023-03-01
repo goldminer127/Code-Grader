d06b53482041412b8bc831dd1e480b4ac48303be
Matthew C <mcheung3@albany.edu>
2023-02-28 21:51:02 -0500

    added typing to some vars

--- front-end/code-grader-ui/src/environments/environments.ts
+++ front-end/code-grader-ui/src/environments/environments.ts
@@ -2,5 +2,13 @@ export const environment = {
     cognito: {
         userPoolId: 'us-east-1_xQ5BclS2c',
         userPoolWebClientId: '61qo1lqgtogms1m4nno70tb3hv',
+    },
+    storage: {
+        bucket: 'studentcodegrader-files',
+        region: 'us-east-1'
+    },
+    auth: {
+        identityPoolId: 'us-east-1:202013df-789b-4ce7-8f0a-617017c9a11e',
+        region: 'us-east-1'
     }
 }
